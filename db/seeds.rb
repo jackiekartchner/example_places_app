f614ae619df9a66af210aaea5ee6b98850399c30
jackie <jacqueline.kartchner@gmail.com>
2019-10-08 10:39:18 -0700

    adds routes

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Place.create(name: "Jackie", address: "3590 FLorian St.")
+Place.create(name: "Aubrey", address: "321 Stanford Ct.")
+Place.create(name: "Julie", address: "580 Dowling St.")
+Place.create(name: "Morgan", address: "4550 Sterling Ct.")
